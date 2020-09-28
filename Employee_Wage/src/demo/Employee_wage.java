d5d7958773e53e377f5f2743b8bfec7a810951ff
RishanJR <rishanrajkumar@yahoo.com>
2020-09-28 21:45:43 +0400

    Add: Checking if employee is present or absent

--- Employee_Wage/src/demo/Employee_wage.java
+++ Employee_Wage/src/demo/Employee_wage.java
@@ -1,10 +1,18 @@
 package demo;
 
 public abstract class Employee_wage {
-
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation");
+	
+		// TYPE CASTING - To check if employee is present or absent
+		int employeeCheck1 = (int) (Math.floor(Math.random()*10)%2);
+	
+		if(employeeCheck1==0)
+			System.out.println("\nEmployee is present");
+		else
+			System.out.println("\nEmployee is absent");
 	}
 
 }
