75f95756c81322c9fd71fe16240c412ec046dbed
RishanJR <rishanrajkumar@yahoo.com>
2020-09-28 22:42:22 +0400

    Add: Checking the monthly wages of the employee with days and number of hours as constraints

--- Employee_Wage/src/demo/Employee_wage.java
+++ Employee_Wage/src/demo/Employee_wage.java
@@ -7,6 +7,7 @@ public abstract class Employee_wage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int MAX_WORK_DAYS = 20;
+	public static final int MAX_HOURS_WORKED = 100;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,9 +16,11 @@ public abstract class Employee_wage {
 		int daily_wage=0;
 		int total_wage=0;
 		int number_of_days = 0;
+		int total_hours_worked=0;
+		
 		System.out.println("Welcome to employee wage computation");
 		
-		while(number_of_days<20) {
+		while(number_of_days<MAX_WORK_DAYS && total_hours_worked<MAX_HOURS_WORKED) {
 			// TYPE CASTING - To check if employee worked full time, part time or not
 			int employeeCheck1 = (int) (Math.floor(Math.random()*10)%3);
 	
@@ -41,8 +44,11 @@ public abstract class Employee_wage {
 			System.out.println("The daily wage of the employee is "+daily_wage);
 			total_wage=total_wage+daily_wage;
 			number_of_days++;
+			total_hours_worked=total_hours_worked+total_hours;
 		}
 
 		System.out.println("\nThe total wages earned by the employee is "+total_wage);
+		System.out.println("The total hours worked in the month is "+total_hours_worked);
+		System.out.println("The number of days worked is "+number_of_days);
 	}	
 }
