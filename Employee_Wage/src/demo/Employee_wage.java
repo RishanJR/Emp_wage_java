b4390fb0cefe29a6ac93f3dbc3c6d3259d4a7b51
RishanJR <rishanrajkumar@yahoo.com>
2020-09-28 21:35:33 +0400

    Welcome to Employee wage computation using java

---
+++ Employee_Wage/src/demo/Employee_wage.java
@@ -0,0 +1,10 @@
+package demo;
+
+public abstract class Employee_wage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to employee wage computation");
+	}
+
+}
