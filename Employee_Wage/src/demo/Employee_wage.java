1ab8871c8e369081dbf391997769fbafc320d721
RishanJR <rishanrajkumar@yahoo.com>
2020-09-29 14:24:59 +0400

    Refactor: Computing employee wage using class methods

--- Employee_Wage/src/demo/Employee_wage.java
+++ Employee_Wage/src/demo/Employee_wage.java
@@ -9,21 +9,20 @@ public abstract class Employee_wage {
 	public static final int MAX_WORK_DAYS = 20;
 	public static final int MAX_HOURS_WORKED = 100;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void Compute_wage() {
 		//Variables
 		int total_hours=0;
 		int daily_wage=0;
 		int total_wage=0;
 		int number_of_days = 0;
 		int total_hours_worked=0;
-		
+				
 		System.out.println("Welcome to employee wage computation");
-		
+				
 		while(number_of_days<MAX_WORK_DAYS && total_hours_worked<MAX_HOURS_WORKED) {
 			// TYPE CASTING - To check if employee worked full time, part time or not
 			int employeeCheck1 = (int) (Math.floor(Math.random()*10)%3);
-	
+			
 			//Solving using switch case
 			switch(employeeCheck1) {
 			case IS_FULL_TIME :
@@ -39,16 +38,20 @@ public abstract class Employee_wage {
 				total_hours=0;
 				break;
 			}
-	
+			
 			daily_wage=total_hours*EMP_RATE_PER_HOUR;
 			System.out.println("The daily wage of the employee is "+daily_wage);
 			total_wage=total_wage+daily_wage;
 			number_of_days++;
 			total_hours_worked=total_hours_worked+total_hours;
 		}
-
 		System.out.println("\nThe total wages earned by the employee is "+total_wage);
 		System.out.println("The total hours worked in the month is "+total_hours_worked);
 		System.out.println("The number of days worked is "+number_of_days);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compute_wage();
 	}	
 }
